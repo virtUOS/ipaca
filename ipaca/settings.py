e7a864a62177f4a499592d750b95bc9648bde52f
Erik Nickel <ernickel@uos.de>
2022-06-03 09:31:33 +0200

    init learning-environment

--- ipaca/settings.py
+++ ipaca/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'learning_environment',
+    'django_bootstrap5',
+    'debug_toolbar',
+
 ]
 
 MIDDLEWARE = [
