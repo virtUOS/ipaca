961d1e9dacc2af61a2d032a52e2edad462bdf6c9
Erik Nickel <ernickel@uos.de>
2022-06-03 10:29:27 +0200

    very basic html template

--- learning_environment/views.py
+++ learning_environment/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    page='index' # for highlighting current page
+    return render(request, 'learning_environment/index.html', locals())
