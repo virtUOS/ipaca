57616459e6400e7ba8698d217b0c7bf131bf6d1e
Erik Nickel <ernickel@uos.de>
2022-06-03 09:45:43 +0200

    changed to more recent django-bootstrap-v5 py package

--- ipaca/settings.py
+++ ipaca/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'learning_environment',
-    'django_bootstrap5',
+    'bootstrap5',
     'debug_toolbar',
 
 ]
