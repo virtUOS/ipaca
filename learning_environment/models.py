a4e97c343591edefb3f6649cb56d3eb2adb1524a
Erik Nickel <ernickel@uos.de>
2022-06-16 10:47:45 +0200

    model improvements, first work on views

--- learning_environment/models.py
+++ learning_environment/models.py
@@ -1,20 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Learner(models.Model):
-    """
-    BasicUser of the learning environment
-    """
-
-
-    user_id = models.CharField(max_length=255)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    competencies = models.ManyToManyField(Competency, through='LearnerCompetency')
-
-    def __str__(self):
-        return self.user_id
-
 
 class Competency(models.Model):
     """
@@ -27,6 +13,19 @@ class Competency(models.Model):
     def __str__(self):
         return self.name
 
+
+class Learner(models.Model):
+    """
+    BasicUser of the learning environment
+    """
+
+    user_id = models.CharField(max_length=255)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    competencies = models.ManyToManyField(Competency, through='LearnerCompetency')
+
+    def __str__(self):
+        return self.user_id
 
 
 class LearnerCompetency(models.Model):
@@ -41,9 +40,51 @@ class LearnerCompetency(models.Model):
     proficiency = models.IntegerField(default=0)
 
 
+class TaskType(models.Model):
+    """
+    Model for capturing the type of the respective task, e.g. single choice, cloze, multiple choice,
+
+    """
+    TASK_TYPES = [
+        ('sc', 'single choice'),
+        ('mc', 'multiple choice')
+    ]
+
+    name = models.CharField(max_length=250, choices=TASK_TYPES)
 
 
+class Text(models.Model):
+    TEXT_TYPES = [
+        ('TB', 'Textbook'),
+        ('JA', 'Journal Article')
+    ]
 
+    title = models.CharField(max_length=250)
+    topic = models.CharField(max_length=250)
+    type = models.CharField(max_length=250, choices=TEXT_TYPES)
+    author = models.CharField(max_length=250)
+    source = models.CharField(max_length=250)
+    content = models.TextField(max_length=100000)
+    license = models.CharField(max_length=250)
+
+
+class Task(models.Model):
+    """
+    Instance of a complete task
+
+    """
+
+    type = models.ManyToManyField(TaskType)
+    competency = models.ManyToManyField(Competency, through='CompetencyTask')
+    difficulty = models.IntegerField()
+
+    text = models.ForeignKey(Text, on_delete=models.CASCADE)
+
+    options = models.CharField(max_length=100)
+    solution = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.content
 
 
 class CompetencyTask(models.Model):
@@ -53,45 +94,8 @@ class CompetencyTask(models.Model):
     Info about the level of the competency
     """
 
-
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     competency = models.ForeignKey(Competency, on_delete=models.CASCADE)
 
     # Within this competency which level is this?
-    level = models.IntegerField() #TODO determine Field type numerical or character for CEFR
-
-class TaskType(models.Model):
-
-    """
-    Model for capturing the type of the respective task, e.g. cloze, multiple choice,
-
-    """
-
-
-
-    name = models.CharField(max_length=100)
-
-
-
-
-
-class Task(models.Model):
-
-    """
-    Instance of a complete task
-
-    """
-
-
-    type = models.ManyToManyField(TaskType)
-    competency = models.ManyToManyField(Competency, through='CompetencyTask')
-    difficulty = models.IntegerField()
-
-    content = models.TextField(max_length=10000)
-    options = models.CharField(max_length=100)
-    solution = models.CharField(max_length=100)
-
-
-    def __str__(self):
-        return self.content
-
+    level = models.IntegerField()  # TODO determine Field type numerical or character for CEFR
--- learning_environment/views.py
+++ learning_environment/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
+from .models import *
+from django.views.generic.edit import FormView
+
+
+#TODO decide which task gets displayed
+
+
+class Task()
+    def __int__(self):
+        pass
+
+def task(request):
+    context = {
+        'title': Task.title,
+        'topic': Task.topic,
+        'type': Task.type,
+        'author': Task.author,
+        'source': Task.source,
+        'content': Task.content,
+        'license': Task.license
+    }
+
+    return render(request, 'learning_environment/index.html', context=context)
+
 
 # Create your views here.
 def index(request):
-    page='index' # for highlighting current page
+    page = 'index'  # for highlighting current page
     return render(request, 'learning_environment/index.html', locals())
