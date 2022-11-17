a964bcabbc336834f23b62dacc6c589a8bdcfad5
Furrii <fpenzkofer@uos.de>
2022-11-17 17:54:53 +0100

    noun_phrase_extraction

--- learning_environment/utils/pipeline.py
+++ learning_environment/utils/pipeline.py
@@ -2,18 +2,25 @@ from .json5_template import template, task
 import json5
 from pathlib import Path
 from .nlp_models import NLP_Model
+from textblob import TextBlob
 
 
 class AutomaticJson():
     nlp_model = NLP_Model()
 
     @staticmethod
-    def text_to_json5(text, answer=None):
+    def noun_phrase_extraction(text):
+        text = TextBlob(text)
+        return text.noun_phrases
+
+    @classmethod
+    def text_to_json5(cls, text, answer=None):
 
         # read in text
         # text = "this is an example text. Let's create a question out of this."
         #text = "I just returned from the greatest summer vacation! It was so fantastic, I never wanted it to end. I spent eight days in Paris, France. My best friends, Henry and Steve, went with me. We had a beautiful hotel room in the Latin Quarter, and it wasn’t even expensive. We had a balcony with a wonderful view."
 
+        AutomaticJson.text = text
         # select sentence to generate question
         ## for now, we choose a random noun
         answer = 'Skills'
@@ -44,5 +51,6 @@ class AutomaticJson():
             json5.dump(template, fp)
 
         print()
+
 
 
