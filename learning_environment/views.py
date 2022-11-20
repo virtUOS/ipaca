c09abafc375da4496cd7ae58ac5f6fda08cdbf2f
aderiyeva <38299876+aderiyeva@users.noreply.github.com>
2022-11-21 00:29:10 +0100

    bar chart previously completed tasks

--- learning_environment/views.py
+++ learning_environment/views.py
@@ -14,7 +14,11 @@ from .forms import *
 from .models import Lesson, Task, Solution, Profile, ProfileSeriesLevel
 from .its.tutormodel import Tutormodel, NoTaskAvailableError
 from .its.learnermodel import Learnermodel
-
+import json
+import time
+import datetime
+from itertools import groupby
+from operator import itemgetter
 
 class SignUpView(SuccessMessageMixin, generic.CreateView):
     form_class = CustomUserCreationForm
@@ -218,7 +222,346 @@ def global_dashboard(request):
         tasks_correctness = correct_solutions / num_solutions * 100.0
     else:
         tasks_correctness = 0.0
-    return render(request, 'learning_environment/global_dashboard.html', locals())  # pass all local variable to template
+
+    # Extract only the timestamp/solved values and convert to list
+    solutions_data = list(solutions.values('timestamp', 'solved'))
+    
+    # random data generation (delete_later)
+    for i in range(1500):
+        import random
+        import datetime
+        start = datetime.datetime.strptime('1/1/2020 1:30 PM', '%m/%d/%Y %I:%M %p')
+        end = datetime.datetime.strptime('1/1/2023 4:50 AM', '%m/%d/%Y %I:%M %p')
+        delta = end - start
+        int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+        random_second = random.randrange(int_delta)
+        randDate = start + datetime.timedelta(seconds=random_second)
+        randSolved = bool(random.getrandbits(1))
+        data_entry = {"timestamp": randDate, "solved": randSolved}
+        solutions_data.append(data_entry)
+    
+    # Group data by days and count the correct/incorrect solutions
+    grouped_data = {}
+    for data in solutions_data:
+        # Convert datetime to YEAR-MONTH-DAY string
+        day_string = data["timestamp"].strftime("%Y-%m-%d")
+        data["timestamp"] = day_string
+
+        # Init dictionary if not already present
+        if day_string not in grouped_data: 
+            grouped_data[day_string] = {'num_correct': 0, 'num_incorrect': 0}
+
+        # Count correct/incorrect solutions 
+        if data['solved']:
+            grouped_data[day_string]["num_correct"] += 1
+        else:
+            grouped_data[day_string]["num_incorrect"] += 1
+
+    # Reorder the grouped data by correct/incorrect for anychart graph
+    correct_solutions_data = []
+    incorrect_solutions_data = []
+    for timestamp_day in grouped_data:
+        correct_entry = {}
+        correct_entry["x"] = timestamp_day  
+        correct_entry["value"] = grouped_data[timestamp_day]["num_correct"]
+        correct_solutions_data.append(correct_entry)
+
+        incorrect_entry = {}
+        incorrect_entry["x"] = timestamp_day
+        incorrect_entry["value"] = grouped_data[timestamp_day]["num_incorrect"]
+        incorrect_solutions_data.append(incorrect_entry)
+
+    # Create series for column graph
+    correct_series = {
+        "name": "Correct",
+        "enabled": True,
+        "seriesType": "column",
+        "clip": True,
+        "isVertical": False,
+        "xMode": "ordinal",
+        "xScale": 0,
+        "yScale": 1,
+        "data": correct_solutions_data
+    }
+    incorrect_series= {
+        "name": "Incorrect",
+        "enabled": True,
+        "seriesType": "column",
+        "clip": True,
+        "isVertical": False,
+        "xMode": "ordinal",
+        "xScale": 0,
+        "yScale": 1,
+        "data": incorrect_solutions_data
+    }
+
+    # Create JSON chart config
+    # from example: https://www.anychart.com/products/anychart/gallery/Column_Charts/Stacked_Column_Chart.php
+    chart = {
+        "chart": {
+            "container": "chart_container",
+            "type": "column",
+            "zIndex": 0,
+            "enabled": True,
+            "title": "Test",
+            "baseline": 0,
+            "barGroupsPadding": 0.8,
+            "barsPadding": 0.4,
+            "xScale": 0,
+            "yScale": 1,
+            "palette": {
+                "type": "distinct",
+                "items": [
+                    "#8fce00",
+                    "#f44336"
+                ]
+            },
+            "series":[
+                correct_series,
+                incorrect_series
+            ],
+            "xAxes": [
+                {
+                    "enabled": True,
+                    "drawFirstLabel": True,
+                    "drawLastLabel": True,
+                    "overlapMode": "no-overlap",
+                    "stroke": "#CECECE",
+                    "orientation": "bottom",
+                    "title": {
+                        "fontSize": 13,
+                        "fontFamily": "Verdana, Helvetica, Arial, sans-serif",
+                        "fontColor": "#545f69",
+                        "fontOpacity": 1,
+                        "vAlign": "top",
+                        "hAlign": "center",
+                        "align": "center",
+                        "text": "Date",
+                    },
+                    "labels": {
+                        "zIndex": 35,
+                        "enabled": True
+                    },
+                    "minorLabels": {
+                        "zIndex": 35,
+                        "enabled": True
+                    },
+                    "ticks": {
+                        "zIndex": 35,
+                        "enabled": True,
+                        "stroke": "#CECECE",
+                        "length": 6,
+                        "position": "outside"
+                    },
+                    "minorTicks": {
+                        "zIndex": 35,
+                        "enabled": True,
+                        "stroke": "#EAEAEA",
+                        "length": 4,
+                        "position": "outside"
+                    },
+                    "scale": 0
+                }
+            ],
+            "yAxes": [
+                {
+                    "enabled": True,
+                    "drawFirstLabel": True,
+                    "drawLastLabel": True,
+                    "overlapMode": "no-overlap",
+                    "stroke": "#CECECE",
+                    "orientation": "left",
+                    "title": {
+                        "zIndex": 35,
+                        "enabled": True,
+                        "fontSize": 13,
+                        "fontFamily": "Verdana, Helvetica, Arial, sans-serif",
+                        "fontColor": "#545f69",
+                        "fontOpacity": 1,
+                        "vAlign": "top",
+                        "hAlign": "center",
+                        "align": "center",
+                        "text": "Num Solutions",
+                    },
+                    "labels": {
+                        "zIndex": 35,
+                        "enabled": True,
+                        "format": "{%Value}{groupsSeparator: }"
+                    },
+                    "minorLabels": {
+                        "zIndex": 35,
+                        "enabled": False
+                    },
+                    "ticks": {
+                        "zIndex": 35,
+                        "enabled": True,
+                        "stroke": "#CECECE",
+                        "length": 6,
+                        "position": "outside"
+                    },
+                    "minorTicks": {
+                        "zIndex": 35,
+                        "enabled": False,
+                        "stroke": "#EAEAEA",
+                        "length": 4,
+                        "position": "outside"
+                    },
+                    "scale": 1
+                }
+            ],
+            "scales": [
+                {
+                    "type": "date-time",
+                    "inverted": False,
+                    "ticks": {
+                        "interval": "P1Y"
+                    },
+                    "minorTicks": {
+                        "interval": "P1M"
+                    },
+                    "mode": "discrete"
+                },
+                {
+                    "type": "linear",
+                    "inverted": False,
+                    "maximum": None,
+                    "minimum": None,
+                    "minimumGap": 0.1,
+                    "maximumGap": 0.1,
+                    "softMinimum": 0,
+                    "softMaximum": None,
+                    "alignMinimum": True,
+                    "alignMaximum": True,
+                    "maxTicksCount": 1000,
+                    "ticks": {
+                        "mode": "linear",
+                        "base": 0,
+                        "allowFractional": True,
+                        "minCount": 4,
+                        "maxCount": 6
+                    },
+                    "minorTicks": {
+                        "mode": "linear",
+                        "base": 0,
+                        "allowFractional": True,
+                        "count": 5
+                    },
+                    "stackMode": "value",
+                    "stackDirection": "direct",
+                    "stickToZero": True,
+                    "comparisonMode": "none"
+                }
+            ],
+            "xScroller": {
+                "zIndex": 35,
+                "enabled": True,
+                "height": 16,
+                "minHeight": None,
+                "maxHeight": None,
+                "orientation": "bottom",
+                "inverted": False,
+                "autoHide": False,
+                "fill": "#f7f7f7",
+                "selectedFill": "#ddd",
+                "outlineStroke": "none",
+                "allowRangeChange": True,
+                "thumbs": {
+                    "normal": {
+                        "fill": "#E9E9E9",
+                        "stroke": "#7c868e"
+                    },
+                    "hovered": {
+                        "fill": "#ffffff",
+                        "stroke": "#757575"
+                    },
+                    "enabled": True,
+                    "autoHide": False
+                },
+                "position": "after-axes"
+            },
+            "xZoom": {
+                "startValue": "2022-1-1",
+                "endValue": "2023-1-1",
+                "continuous": True
+            },
+            "legend": {
+                "zIndex": 200,
+                "enabled": True,
+                "fontSize": 13,
+                "fontFamily": "Verdana, Helvetica, Arial, sans-serif",
+                "fontColor": "#7c868e",
+                "fontOpacity": 1,
+                "vAlign": "bottom",
+                "hAlign": "start",
+                "textOverflow": "...",
+                "selectable": False,
+                "disablePointerEvents": False,
+                "useHtml": False,
+                "inverted": False,
+                "itemsLayout": "horizontal",
+                "iconSize": 15,
+                "position": "top",
+                "positionMode": "outside",
+                "drag": False,
+                "itemsHAlign": "left",
+                "itemsSpacing": 15,
+                "itemsSourceMode": "default",
+                "hoverCursor": "pointer",
+                "iconTextSpacing": 5,
+                "align": "center",
+                "margin": {
+                    "left": 0,
+                    "top": 0,
+                    "bottom": 0,
+                    "right": 0
+                },
+                "padding": {
+                    "left": 0,
+                    "top": 0,
+                    "bottom": 20,
+                    "right": 0
+                },
+                "paginator": {
+                    "zIndex": 30,
+                    "enabled": True,
+                    "fontSize": 12,
+                    "fontFamily": "Verdana, Helvetica, Arial, sans-serif",
+                    "fontColor": "#545f69",
+                    "fontOpacity": 1,
+                    "vAlign": "top",
+                    "hAlign": "start",
+                    "textOverflow": "",
+                    "selectable": False,
+                    "disablePointerEvents": False,
+                    "useHtml": False,
+                    "orientation": "right",
+                    "layout": "horizontal",
+                    "padding": {
+                        "left": 5,
+                        "top": 0,
+                        "bottom": 0,
+                        "right": 0
+                    },
+                    "margin": {
+                        "left": 0,
+                        "top": 0,
+                        "bottom": 0,
+                        "right": 0
+                    }
+                },
+            },
+    
+        }
+    }
+    context = {
+        "chartData": json.dumps(chart), 
+        "solutions": solutions, 
+        "num_solutions": num_solutions,
+        "correct_solutions": correct_solutions,
+        "tasks_correctness": tasks_correctness
+    }
+
+    return render(request, 'learning_environment/global_dashboard.html', context)
 
 
 def learner_reset(request):
