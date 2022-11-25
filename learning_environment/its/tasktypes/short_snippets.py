80453043fc59def9dfe24043df52b9007b87310b
emzSid <emilly.sidaine@gmx.de>
2022-11-25 14:31:50 +0100

    is not correct feedback in smart_snippets

--- learning_environment/its/tasktypes/short_snippets.py
+++ learning_environment/its/tasktypes/short_snippets.py
@@ -1,6 +1,6 @@
 from learning_environment.its.base import Json5ParseException
-#nltk.download('punkt')
 import nltk 
+#nltk.download('punkt')
 from nltk.stem import WordNetLemmatizer
 from spellchecker import SpellChecker
 from happytransformer import HappyTextToText, TTSettings
@@ -26,37 +26,41 @@ class ShortTask():
     def __init__(self, task):
         self.task = task
         self.happy_tt = HappyTextToText("T5", "vennify/t5-base-grammar-correction")
-        #self.happy_wp = HappyWordPrediction()
         self.beam_settings = TTSettings(num_beams=5, min_length=1, max_length=100)
 
     def analyze_solution(self, solution):
         context = {}
         analysis = {}
-        word_snippets = ["It", "is", "funnier", "the", "funniest", "tree", "task"]
-        
+
         #counts the errortpes
         errortypes = {'spelling': 0, 'grammar': 0, 'length': 0}
 
-        # gives the words's position in which the error type ocurrs
-        # !starts at 0
-        s_error = {}
-
+        
+        
 
 
         #snippets: It, are, funny, the, funniest, is, funnier, task
         #User input: It is funnier task
         user_answer = solution.get('answer', None)
 
+        # get the snippets to use
+        word_snippets_str = self.task.question.replace(" ", "")
+        word_snippets = word_snippets_str.split("/")
+        
+        print(word_snippets)
+
+        #make sure it ends with a dot
         if not user_answer.endswith('.'):
             user_answer = user_answer +"."
 
-
-        print("user: "+ user_answer)
     
         # tokenize: ["It", "is", "funnier", "task"]
         tokenized_user_answer = nltk.word_tokenize(user_answer)
 
-        
+        # gives the words's position in which the error type ocurrs
+        # !starts at 0
+        s_error = dict.fromkeys(tokenized_user_answer)
+
         #Spelling Correction
         for i in range(len(tokenized_user_answer)):
             tok_word = tokenized_user_answer[i]
@@ -68,12 +72,11 @@ class ShortTask():
                 tokenized_user_answer[i] = corr_word
                 user_answer = user_answer.replace(tok_word, corr_word)
                 errortypes['spelling'] += 1
-                s_error[i] = 'spelling_error'
-                print("corrected_user_answer: " + user_answer)
+                s_error[tok_word] = 'spelling_error'
 
         #generate right answer with happy transformer
         right_answer = self.happy_tt.generate_text(user_answer, args=self.beam_settings).text
-        print("right: ")
+        print("corrected: ")
         print(right_answer)
         tokenized_right_answer = nltk.word_tokenize(right_answer)
 
@@ -84,7 +87,7 @@ class ShortTask():
                 right_word = tokenized_right_answer[i]
                 if right_word != user_word:
                         errortypes['grammar'] += 1
-                        s_error[i] = 'grammar_error'
+                        s_error[user_word] = 'grammar_error'
         
         else:
             errortypes['length'] += 1
@@ -99,12 +102,8 @@ class ShortTask():
         #lemmatize: 
         lemmatized_user_answer = [lemmatizer.lemmatize(w.lower()) for w in tokenized_user_answer]
         lemmatized_right_answer = [lemmatizer.lemmatize(w.lower()) for w in tokenized_right_answer]
-
-        print("lem_user: ")
-        print(lemmatized_user_answer)
-        print("lem_right: ")
         print(lemmatized_right_answer)
-
+        print(lemmatized_user_answer)
         #check whether all words in the solution are also in the user solution
         snippets_in = 0
         enough_words_used = True
@@ -125,17 +124,18 @@ class ShortTask():
             analysis['solved'] = True
         
         else:  
-            for errortype in s_error:
-                if errortype > 0:
-                    feedback = "You had an error"
-                    analysis['solved'] = False
-                    context['mode'] = "result" 
-        
+            #underlined_word = "\u0332".join(word + " ")
+            feedback = "You had an error"
+            analysis['solved'] = False
+            context['mode'] = "result" 
+
         context['feedback'] = feedback # display to try again 
-                
 
+        corrected_user_underlined = ""
+        for word in s_error:
+            corrected_user_underlined += word + " "
 
-         #It is the funniest task
+        context['corrected_user_underlined'] = corrected_user_underlined        
 
         return (analysis, context)
 
