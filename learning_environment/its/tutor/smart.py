93083bd2e67a7dc9a9e99bec70338c1d11f91f2c
KaiDoennebrink <kai@doennebrink-lh.de>
2022-12-06 10:24:30 +0100

    refactor: Adds comments and changes data types

--- learning_environment/its/tutor/smart.py
+++ learning_environment/its/tutor/smart.py
@@ -1,11 +1,12 @@
+import random
 from typing import Tuple, List, Dict, Any
 
+from datetime import timedelta
 from django.db.models import Count
+from django.utils.timezone import localtime
 
 from learning_environment.its.tutor import BaseTutorModel
 from learning_environment.models import Task, Solution
-import time
-import random
 
 
 class SmartTutorModel(BaseTutorModel):
@@ -24,8 +25,8 @@ class SmartTutorModel(BaseTutorModel):
         else:
             user_levels = self._get_user_levels(next_type)
 
+            # get the position of the user in all user levels as a probability
             current_user_level = 0
-
             for ind, user in enumerate(user_levels):
                 if user["id"] == self.learner.id:
                     current_user_level = ind / len(user_levels)
@@ -34,19 +35,24 @@ class SmartTutorModel(BaseTutorModel):
             # select task id accordingly to skill of user and estimated task difficulty
             index = int((1 - current_user_level) * len(task_difficulties) - 1)
             task_id = task_difficulties[index]["id"]
-            while not self._deteremine_task_probability(task_id, request.user):
-                if index + 1 > len(task_difficulties) - 1:
-                    break
-                index += 1
+
+            # check whether the task should repeated otherwise choose the next task
+            while not self._should_task_be_repeated(task_id, request.user):
+                index = (index + 1) % len(task_difficulties)
                 task_id = task_difficulties[index]["id"]
 
             task = Task.objects.get(pk=task_id)
-
 
         return next_type, task
 
     @staticmethod
     def _get_task_difficulties(lesson, next_type) -> List[Dict[str, Any]]:
+        """
+        Calculates the difficulty of each task of a specific type in a lesson. Therefore it is checked how often each
+        task has been done and how often the answer was correct to calculate the share of correct answers. The tasks
+        are then sorted by the share of correct answers and then returned.
+        Example: [{'id':4, 'share':0.1}, {'id':3, 'share':0.4}, ..., {'id':5, 'share':0.7}]
+        """
         tasks = Task.objects.filter(lesson=lesson, type=next_type)
         task_difficulties = []
 
@@ -63,21 +69,33 @@ class SmartTutorModel(BaseTutorModel):
         return task_difficulties
 
     @staticmethod
-    def _deteremine_task_probability(task_id, user_id):
+    def _should_task_be_repeated(task_id: str, user_id: str) -> bool:
+        """
+        Checks whether the given task should be repeated. The task is repeated if the last repetition was more than a
+        week ago or if the probability of repetition is good. The probability increases with time.
+        """
         try:
             last_solution = Solution.objects.filter(task=task_id, user=user_id).order_by('timestamp')[0]
         except IndexError:
             return True
-        timestamp = last_solution.timestamp
-        zEI_T = time.time() - 604800 # 1 week in seconds
-        timestamp = (timestamp - zEI_T) / 604800
-        if random.randint(0, 100) < 100 * timestamp:
-            return True
-        return False
+
+        # calculate the probability to repeat the task
+        one_week_delta = timedelta(weeks=1)
+        last_solution_timestamp = last_solution.timestamp
+        comparing_timestamp = localtime() - one_week_delta
+        repeat_task_probability = (last_solution_timestamp - comparing_timestamp) / one_week_delta
+
+        return random.random() < repeat_task_probability
 
     @staticmethod
-    def _get_user_levels(next_type: str):
-        query_set = Solution.objects.values('user', 'solved').filter(task__type=next_type).annotate(count=Count('id')).order_by('user', 'solved')
+    def _get_user_levels(task_type: str) -> List[Dict[str, Any]]:
+        """
+        Calculates all user levels for a given task type. Therefore it checks how each user has performed in tasks with
+        the given task typ and calculates the correct share. The users are then sorted by this share and afterwards
+        returned.
+        Example: [{'id':8, 'share':0.51}, {'id':1, 'share':0.54}, ..., {'id':10, 'share':0.8}]
+        """
+        query_set = Solution.objects.values('user', 'solved').filter(task__type=task_type).annotate(count=Count('id')).order_by('user', 'solved')
         levels = []
 
         correct_answers = 0
